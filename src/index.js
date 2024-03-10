e464fa55e3d1124e4c5ad0ba8312eded303cd0a1
michaelr3207 <michael--robinson321@hotmail.com>
2024-03-10 19:40:44 +0000

    webpack and html setup

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import  './style.css';
+
+const API_KEY = '9118f127dc5a4c16b72193619241003';
+
+// console.log(https://api.weatherapi.com/v1/current.json?key=9118f127dc5a4c16b72193619241003&q=london
+// )
+
