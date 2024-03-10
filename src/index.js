9dae173cf2de6d6a6e37dc08566f4c11108aa03e
michaelr3207 <michael--robinson321@hotmail.com>
2024-03-10 19:53:53 +0000

    api started

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import  './style.css';
 
 const API_KEY = '9118f127dc5a4c16b72193619241003';
+const API_URL = 'https://api.weatherapi.com/v1/current.json?key=9118f127dc5a4c16b72193619241003&q=';
+let currentSubject = 'london';
 
 // console.log(https://api.weatherapi.com/v1/current.json?key=9118f127dc5a4c16b72193619241003&q=london
 // )
 
+async function generateWeatherData() {
+    try {
+        const img = document.querySelector('img');
+        const response = await fetch(API_URL + currentSubject, {mode: 'cors'});
+        const fetchedData = await response.json();
+        console.log(fetchedData);
+        alert(fetchedData.location.country);
+    }
+    catch (error) {
+        alert('ERROR!');
+        console.log(error.toString());
+    }
+}
+
+generateWeatherData();
